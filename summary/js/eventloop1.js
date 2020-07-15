ee8e6dd2d5206ec995bec1a777a2e646365a95ca
wang_jian <wang_jian@ctrip.com>
2020-07-15 16:03:13 +0800

    sginit

---
+++ summary/js/eventloop1.js
@@ -0,0 +1,19 @@
+//1 普通异步
+
+console.log(1);
+setTimeout(() => {
+    console.log(2)
+}, 0);
+setTimeout(() => {
+    console.log(3)
+}, 3000);
+new Promise((resolve, rejest) => {
+    console.log(4)
+    setTimeout(() => {
+        console.log(5)
+    }, 3000);
+    resolve(6)
+}).then(res => {
+    console.log(res)
+});
+console.log(7)
---
+++ summary/js/eventloop2.js
@@ -0,0 +1,28 @@
+
+
+//2 asyn await
+console.log(1)
+async function A() {
+    console.log(2)
+    await B()
+    console.log(3)
+}
+async function B() {
+    console.log(4)
+}
+A()
+new Promise(function (resolve) {
+    console.log(5)
+    resolve()
+}).then(res => console.log(6))
+
+//这里的A() 等价于
+// new Promise(resolve1 => {
+//     console.log(2);
+//     new Promise((resolve2) => {
+//         console.log(4);
+//         resolve2();
+//     }).then(val => {
+//         console.log(3)
+//     })
+// });
